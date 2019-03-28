bf3721324e19303ec7abda42eaf7d67c4d2f7713
Bamdres16 <brianm.bra.mm8@gmail.com>
2019-03-27 22:02:06 -0600

    Consultas

--- Gramatica.pl
+++ Gramatica.pl
@@ -1,19 +1,20 @@
+start(Oracion):- split_string(Oracion, " ", "", L), phrase(inicio, L).
+start(Oracion):- split_string(Oracion, " ", "", L), phrase(fin, L).
+start(Oracion):- split_string(Oracion, " ", "", L), phrase(pregunta, L).
 %Saludar
 
 inicio --> saludo(Tipo,Num), nombre.
 saludo(Tipo,Num) --> unica.
 saludo(Tipo,Num) --> primera(Tipo,Num), segunda(Tipo,Num).
 
-unica --> ["Hola"].
-primera(1,plural) --> ["Buenos"].
-primera(1,plural) --> ["buenos"].
+unica --> ["Hola"]; ["hola"].
+primera(1,plural) --> ["Buenos"]; ["buenos"].
 
-primera(1,singular) --> ["Buen"].
-primera(1,singular) --> ["buen"].
 
-primera(2,plural) --> ["Buenas"].
-primera(2,plural) --> ["buenas"].
+primera(1,singular) --> ["Buen"]; ["buen"].
 
+
+primera(2,plural) --> ["Buenas"]; ["buenas"].
 segunda(1,plural) --> ["dias"].
 
 segunda(1,singular) --> ["Dia"].
@@ -21,13 +22,8 @@ segunda(1,singular) --> ["Dia"].
 
 segunda(2,plural) --> ["tardes"].
 segunda(2,plural) --> ["noches"].
-nombre --> ["DrLog"].
-nombre --> ["drlog"].
-nombre --> ["Doctor"].
-nombre --> ["doctor"].
-nombre --> ["Dr"].
-nombre --> ["dr"].
 
+nombre --> ["DrLog"]; ["drlog"]; ["Doctor"]; ["doctor"]; ["Dr"];["dr"].
 
 %Finalizar
 
@@ -42,3 +38,46 @@ d_segunda --> ["gracias"].
 
 
 %Preguntas
+%Pronombre + sujeto + verbo
+pregunta --> pronombre(Num,Tipo), sujeto(Num,Tipo), verbo(Num,Tipo).
+%Pronombre + verbo compuesto
+pregunta --> pronombre(Num,Tipo), verbo(Num,Tipo), verbo(Num,Tipo).
+%Pronombre + verbo + articulo + sujeto
+pregunta --> pronombre(Num,Tipo), verbo(Num,Tipo), articulo(Num,Tipo), sujeto(Num,Tipo).
+%Pronombre + verbo + nombre
+pregunta --> pronombre(Num,Tipo), verbo(Num,Tipo), nombre.
+% Pronombre + verbo + articulo + sujeto + preposicion + articulo +
+% pregunta --> pronombre(Num,Tipo), verbo(Num,Tipo), articulo(Num,Tipo),
+% sujeto(Num,Tipo), preposición, articulo(Num2,Tipo2), enfermedad.
+
+% enfermedad
+pronombre(singular,1) --> ["¿Qué"]; ["Qué"].
+pronombre(plural,2) --> ["¿Cuáles"]; ["Cuáles"].
+pronombre(singular,1) --> ["¿Cuál"]; ["Cuál"].
+pronombre(singular,3) --> ["¿Cómo"];["Cómo"].
+
+
+sujeto(singular,1) --> ["enfermedad"].
+sujeto(plural,2) --> ["causas?"]; ["causas"].
+
+sujeto(singular,1) --> ["tratamiento?"]; ["tratamiento"].
+
+verbo(singular,1) -->  ["tengo?"]; ["tengo"].
+verbo(plural,2) --> ["son"].
+verbo(singular,1) --> ["es"].
+verbo(singular,3) --> ["puedo"].
+verbo(singular,3) --> ["prevenirla?"]; ["prevenirla"].
+
+articulo(plural,2) --> ["las"].
+articulo(singular,1) --> ["el"].
+articulo(singular,1) --> ["la"].
+
+preposición --> ["de"].
+
+%Consultas
+consulta(Base):- pregunta_y_lee(Entrada), procesar_entrada(Entrada, Base).
+
+pregunta_y_lee(Entrada):- write('? '), read(Entrada).
+procesar_entrada(fin,_Base_):- !.
+procesar_entrada(muestra_reglas,Base):- !, muestra_reglas(Base), consulta(Base).
+procesar_entrada(Oracion,Base):- phrase(
